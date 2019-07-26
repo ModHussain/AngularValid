e3722d5ebc4a541b58e4de14a16d80bc6c0f6471
mohammad hussain <mohammad.hussain-ext@st.com>
2019-07-26 18:04:48 +0530

    Angular Validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { User } from './user';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularvalidationexam';
+  user = new User();
+  users = [];
+  bool = false;
+  onSubmit(){
+    console.log(this.user);
+    this.users.push(this.user);
+    this.bool = true;
+    console.log(this.users);
+    this.user = new User();
+  }
 }
